8c966443188eca2ba80aa31a413b3b28a4725de3
natalie <wong.lt.natalie@gmail.com>
2023-09-11 12:30:41 -0400

    c

---
+++ u1/L2_tryAndCatch.java
@@ -0,0 +1,25 @@
+package u1;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class L2_tryAndCatch {
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) { // includes ALL exception
+            // NOT good practice
+        }
+
+        try {
+            Scanner in = new Scanner(new File("input.txt"));
+        } catch (IOException e) {
+
+        } catch (FileNotFound e) { // will NOT work as FileNotFound class is under IOException
+
+        }
+
+        // if catching multiple exceptions, make sure you catch the most specific one
+        // first, and the most exception at last
+    }
+}
--- u1/assignments/C.java
+++ u1/assignments/C.java
@@ -7,8 +7,57 @@
 
 package u1.assignments;
 
-public class C {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.io.*;
 
+public class C {
+    public static void findLargestPalindrome(String s) {
+        String word;
+        String flippedWord = "";
+
+        for (int i = s.length(); i > 0; i--) {
+            for (int n = 0; n <= s.length() - i; n++) {
+
+                word = s.substring(n, n + i);
+                // System.out.println(word);
+
+                for (int x = word.length() - 1; x >= 0; x--) {
+                    flippedWord += word.charAt(x);
+                }
+                // System.out.println(flippedWord);
+
+                if (word.equalsIgnoreCase(flippedWord)) {
+                    System.out.println("Screen Output");
+                    System.out.println("\tFinding the largest palindrome");
+                    System.out.printf("\tLargest palindrome: %s%n", word);
+                    System.out.printf("\tStarting position: %d%n", n + 1);
+                    System.out.printf("\tLength: %d%n%n", word.length());
+
+                    return;
+                }
+                flippedWord = "";
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Variables
+        String s;
+
+        // Main Code
+        try {
+            Scanner inputFile = new Scanner(new File("input.txt"));
+
+            while (inputFile.hasNextLine()) {
+                s = inputFile.nextLine();
+
+                System.out.printf("File input: \n\t%s%n%n", s);
+
+                findLargestPalindrome(s);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found!");
+        }
     }
 }
