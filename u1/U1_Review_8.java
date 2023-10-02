70394e3dd16e446e7e6b4d6dfb74d7195f2a7bae
natalie <wong.lt.natalie@gmail.com>
2023-10-02 16:14:04 -0400

    asdkfjaklsdfjalskj

---
+++ u1/U1_Review_8.java
@@ -0,0 +1,14 @@
+package u1;
+
+import java.math.BigInteger;
+
+public class U1_Review_8 {
+    public static void main(String[] args) {
+        BigInteger first = new BigInteger("34");
+        BigInteger sec = BigInteger.valueOf(30);
+
+        System.out.println(first.compareTo(sec));
+        System.out.println(first.pow(2));
+
+    }
+}
---
+++ u1/U1_Review_9.java
@@ -0,0 +1,23 @@
+package u1;
+
+public class U1_Review_9 {
+    public static void main(String[] args) {
+        // a)
+        // REPETITION: the process of repeating an act
+        // RECURSION: a function which calls itself either directly or indirectly
+
+        // b)
+        // 1. There must be a base/degenerate case(s)
+        // 2. There must be a way to reduce the problem so that it reaches the base
+        // case(s)
+        // 3. There must be a way of achieving the larger result from the smaller
+        // elements
+
+        // c)
+        // StackOverFlowError = when a recursive function gets called
+        // excessively/infinitely to the point where the space to store the
+        // data/information exceeds the amount that can fit on the stack
+        // usually occurs when recurisve functions infinitely call themselves (like an
+        // infinite loop)
+    }
+}
