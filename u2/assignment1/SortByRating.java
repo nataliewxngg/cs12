41ded2206de4fc6b7a5a91f9ccfa3bfc1c240543
natalie <wong.lt.natalie@gmail.com>
2023-10-16 15:22:44 -0400

    commenting

--- u2/assignment1/SortByRating.java
+++ u2/assignment1/SortByRating.java
@@ -3,7 +3,10 @@ package u2.assignment1;
 import java.util.*;
 
 public class SortByRating implements Comparator<Games> {
-    public int compare(Games g1, Games g2) {
+
+    // DESCRIPTION: A compare method utilized for the comparator interface, which
+    // sorts an ArrayList of Games Objects by rating, in ascending order
+    public int compare(Games g1, Games g2) { // PARAMETERS: 2 Games objects
         if (g1.rating == g2.rating)
             return 0;
         else if (g1.rating < g2.rating)
