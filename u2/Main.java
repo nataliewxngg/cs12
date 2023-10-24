fcfbd9ac2aa5639d9fb6d7d0ad2656efcd53f594
natalie <wong.lt.natalie@gmail.com>
2023-10-23 21:13:08 -0400

    regaiuriuleghj;a

---
+++ u2/Main.java
@@ -0,0 +1,10 @@
+package u2;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader in = new BufferedReader(new FileReader());
+    }
+}
