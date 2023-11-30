22e2b8e3807283eb9a3652ca5745351ca6feb288
natalie <wong.lt.natalie@gmail.com>
2023-11-29 21:29:33 -0500

    doing p3

---
+++ u3/assignment5/Problem3.java
@@ -0,0 +1,15 @@
+// Natalie Wong
+// Due Sunday, December 3, 2023
+
+// Assignment #5 - Dynamic Data Structures - PROBLEM #3
+// This program determines the original order of cards, considering they all fall under the same sequence until facing upwards.
+
+package u3.assignment5;
+
+import java.util.*;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
