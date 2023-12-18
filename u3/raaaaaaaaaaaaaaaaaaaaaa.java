4613f01d964d5f3f4386b612414fbe70d5566926
natalie <wong.lt.natalie@gmail.com>
2023-12-18 00:25:45 -0500

    goodluck :)

---
+++ u3/raaaaaaaaaaaaaaaaaaaaaa.java
@@ -0,0 +1,9 @@
+package u3;
+
+import java.util.*;
+
+public class raaaaaaaaaaaaaaaaaaaaaa {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>(Arrays.asList(1, 5, 3, 4, 12));
+    }
+}
