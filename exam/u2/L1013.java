ebe0bc3b1a8dd379cc060dd75a1b9c5de91c2898
natalie <wong.lt.natalie@gmail.com>
2024-01-24 02:36:43 -0500

    u2

---
+++ exam/u2/L1013.java
@@ -0,0 +1,15 @@
+package exam.u2;
+
+import java.util.*;
+
+class L1013 {
+    public static void main(String[] args) {
+        int i = 2;
+        String s = i + "";
+
+        Integer i1 = i;
+        Integer.toString(i1);
+        System.out.println(i1);
+
+    }
+}
